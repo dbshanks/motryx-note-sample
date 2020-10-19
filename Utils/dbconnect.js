d622e753d15f3bef20e54e5892f540e7c5c7f67d
Derek Shanks <dbshanks75@hotmail.com>
2020-10-18 23:19:58 -0300

    _

--- Utils/dbconnect.js
+++ Utils/dbconnect.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const dbConnect = () => {
-  mongoose.connect(process.env.MONGO_URL, {
+  mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
